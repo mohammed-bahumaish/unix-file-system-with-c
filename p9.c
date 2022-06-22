f049b6e9408e08d0c259ee6ee7bab0156fcc5376
mohammed gehad <mohammed.gehad.1998@gmail.com>
2022-06-22 15:16:44 +0530

    init

---
+++ p9.c
@@ -0,0 +1,32 @@
+/* Write a C program to demonstrate race condition among parent and child processes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include<unistd.h>
+static void charatatime(char *);
+int main(void)
+{
+  pid_t pid;
+  if ((pid = fork()) < 0)
+  {
+    printf("fork error");
+  }
+  else if (pid == 0)
+  {
+    charatatime("output from childccccccccccccccccccccccccccccccccc\n");
+  }
+  else
+  {
+    charatatime("output from parentpppppppppppppppppppppppp\n");
+  }
+  exit(0);
+}
+static void charatatime(char *str)
+{
+  char *ptr;
+  int c;
+  setbuf(stdout, NULL); /* set unbuffered */
+  for (ptr = str; (c = *ptr++) != 0;)
+    putc(c, stdout);
+}
